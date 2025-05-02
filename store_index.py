0c4ea6f7a090cefadc18939d45445bad015e8999
Shruti <shrutipatil1424@gmail.com>
2025-05-02 12:47:20 +0530

    updated

---
+++ store_index.py
@@ -0,0 +1,39 @@
+from src.helper import load_pdf_files, text_split, download_hugging_face_embedding
+from pinecone.grpc import PineconeGRPC as Pinecone
+from pinecone import ServerlessSpec
+from langchain_pinecone import PineconeVectorStore
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+PINECONE_API_KEY=os.environ.get('PINECONE_API_KEY')
+os.environ["PINECONE_API_KEY"]= PINECONE_API_KEY
+
+extracted_data=load_pdf_file(data="Data/")
+
+text_chunks=text_split(extracted_data)
+
+embeddings=download_hugging_face_embeddings()
+
+pc= Pinecone(api_key="pcsk_KjGui_u2DMwk4MFrTyrqNupdE38uh3nk6wPePc3TNyPCPEJEceGxrHLyRJYzMduJLu6a")
+
+index_name='medicalbot'
+
+pc.create_index(
+    name=index_name,
+    dimension=384,
+    metric="cosine",
+    spec=ServerlessSpec(
+        cloud="aws",
+        region="us-east-1"
+    )
+)
+
+#Enable each chunk and upsert the embedding into your Pinecone index.
+
+docsearch = PineconeVectorStore.from_documents(
+    documents=text_chunks,
+    index_name=index_name,
+    embedding=embeddings,
+)
